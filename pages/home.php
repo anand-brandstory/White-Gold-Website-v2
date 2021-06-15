3746df06ab00d3b568c579efe9bf5bb4aa3e8475
MarioWindsor <mario@lazaro.in>
2021-06-15 21:47:11 +0530

    Homepage: front-end build: Markup: p1

--- pages/home.php
+++ pages/home.php
@@ -13,18 +13,222 @@ require_once __ROOT__ . '/inc/header.php';
 
 
 
-<!-- Sample Page Content -->
-<section class="intro-section">
+<!-- Sample Section -->
+<section class="sample-section">
 	<div class="container">
 		<div class="row">
-			<div class="columns small-12 space-half-top-bottom">
-				<div class="h2">Welcome.</div>
-				<div class="h3">You are at the source.</div>
+			<div class="columns small-12">
+				<!-- insert text -->
 			</div>
 		</div>
 	</div>
 </section>
-<!-- END: Sample Page Content -->
+<!-- END: Sample Section -->
+
+
+<!-- ## General Page -->
+<!-- Header Section -->
+<section class="header-section">
+	<div class="container">
+		<div class="row">
+			<div class="columns small-12 space-half-top-bottom">
+				<div class="logo">WhiteGold</div>
+				<div class="select-region">Karnataka</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Header Section -->
+
+
+<!-- Menu Section -->
+<!-- END: Menu Section -->
+
+
+<!-- ## Home Page -->
+<!-- Landing Carousel Section -->
+<section class="landing-carousel-section">
+	<div class="container">
+		<div class="row">
+			<div class="columns small-12">
+				<!-- insert text -->
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Landing Carousel Section -->
+
+
+<!-- Sell Gold Form Section -->
+<section class="sell-gold-form-section">
+	<div class="container">
+		<div class="row">
+			<div class="columns small-12">
+				<!-- insert text -->
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Sell Gold Form Section -->
+
+
+<!-- Sell Gold Section -->
+<section class="sell-gold-section">
+	<div class="container">
+		<div class="row">
+			<div class="columns small-12">
+				<!-- insert text -->
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Sell Gold Section -->
+
+
+<!-- Sell Gold Home Visit Form Section -->
+<section class="sell-gold-home-visit-form-section">
+	<div class="container">
+		<div class="row">
+			<div class="columns small-12">
+				<!-- insert text -->
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Sell Gold Home Visit Form Section -->
+
+
+<!-- Sell Gold FAQs Section -->
+<section class="sell-gold-faqs-section">
+	<div class="container">
+		<div class="row">
+			<div class="videos-faqs columns small-12">
+				<!-- insert text -->
+			</div>
+			<div class="articles-faqs columns small-12">
+				<!-- insert text -->
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Sell Gold FAQs Section -->
+
+
+<!-- Report Malpractice Section -->
+<section class="report-malpractice-section">
+	<div class="container">
+		<div class="row">
+			<div class="columns small-12">
+				<!-- insert text -->
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Report Malpractice Section -->
+
+
+<!-- Release Gold Section -->
+<section class="release-gold-section">
+	<div class="container">
+		<div class="row">
+			<div class="columns small-12">
+				<!-- insert text -->
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Release Gold Section -->
+
+
+<!-- Release Gold FAQs Section -->
+<section class="release-gold-faqs-section">
+	<div class="container">
+		<div class="row">
+			<div class="videos-faqs columns small-12">
+				<!-- insert text -->
+			</div>
+			<div class="articles-faqs columns small-12">
+				<!-- insert text -->
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Release Gold FAQs Section -->
+
+<!-- ## Find Branch Page -->
+<!-- Find Branch Section -->
+<section class="find-branch-section">
+	<div class="container">
+		<div class="row">
+			<div class="intro columns small-12">
+				<!-- insert text -->
+			</div>
+			<div class="branch-listing columns small-12">
+				<!-- insert text -->
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Find Branch Section -->
+
+
+<!-- ## Live Gold Page -->
+<!-- Live Gold Section -->
+<section class="live-gold-section">
+	<div class="container">
+		<div class="row">
+			<div class="inline-modal columns small-12">
+				<div class="otp-verify">
+					<!-- insert text -->
+				</div>
+				<div class="off-hrs">
+					<!-- insert text -->
+				</div>
+				<div class="holiday">
+					<!-- insert text -->
+				</div>
+				<div class="number-blocked">
+					<!-- insert text -->
+				</div>
+			</div>
+			<div class="live-gold columns small-12">
+				<div class="row">
+					<div class="live-gold-graph columns small-12">
+						<!-- insert text -->
+					</div>
+					<div class="live-gold-data columns small-12">
+						<!-- insert text -->
+					</div>
+					<div class="live-gold-quote columns small-12">
+						<!-- insert text -->
+					</div>
+					<div class="live-gold-alert-form columns small-12">
+						<!-- insert text -->
+					</div>
+					<div class="live-gold-video columns small-12">
+						<!-- insert text -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Live Gold Section -->
+
+
+
+<!-- Footer Section -->
+<section class="footer-section">
+	<div class="container">
+		<div class="row">
+			<div class="columns small-12 space-half-top-bottom">
+				<div class="footer-content"><!-- insert text --></div>
+				<div class="footer-menu"><!-- insert text --></div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Footer Section -->
 
 
 
