1d8a12b4ff9f47ed6cb52c79df44c794ae428a92
MarioWindsor <mario@lazaro.in>
2021-06-18 18:42:14 +0530

    Homepage: front-end build: p6

---
+++ pages/section/release-gold.php
@@ -0,0 +1,294 @@
+<section class="release-gold-section fill-neutral-1 space-100-top-bottom">
+	<div class="container">
+		<div class="row">
+			<div class="intro columns small-12">
+				<div class="h1 strong text-blue-2">Release your Gold</div>
+				<div class="h1 strong text-neutral-3">Eight easy steps</div>
+			</div>
+			<div class="step-cards columns small-12">
+				<div class="step-card-grid row">
+					<div class="step-break columns small-12 space-75-left-right">
+						<div class="row text-neutral-3 space-75-top">
+							<div class="label strong text-uppercase columns small-6">At home</div>
+							<div class="p strong columns small-6 text-right">&middot;&ensp;&middot;&ensp;&middot;</div>
+						</div>
+					</div>
+					<!-- CARD: 1 -->
+					<div class="card fill-light">
+						<div class="row">
+							<label class="card-head" tabindex="-1">
+								<div class="columns small-1"><span class="index h6 medium text-blue-2">1</span></div>
+								<div class="title h3 strong text-blue-2 columns small-10 space-50-left-right">Find a Branch</div>
+								<div class="toggle columns small-1">
+									<input class="visuallyhidden" type="checkbox">
+									<div class="arrow"><span class="a1"></span><span class="a2"></span></div>
+								</div>
+							</label>
+						</div>
+						<div class="card-content">
+							<div class="row">
+								<div class="columns small-9 small-offset-1 space-50-left-right">
+									<div class="h5 medium space-150-top">49 Branches</div>
+									<div class="h6 medium space-150-bottom">Across Karnataka</div>
+									<a href="" class="button">Find Branch</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- CARD: 2 -->
+					<div class="card fill-light">
+						<div class="row">
+							<label class="card-head" tabindex="-1">
+								<div class="columns small-1"><span class="index h6 medium text-blue-2">2</span></div>
+								<div class="title h3 strong text-blue-2 columns small-10 space-50-left-right">Bring Pledge Receipt</div>
+								<div class="toggle columns small-1">
+									<input class="visuallyhidden" type="checkbox">
+									<div class="arrow"><span class="a1"></span><span class="a2"></span></div>
+								</div>
+							</label>
+						</div>
+						<div class="card-content">
+							<div class="row">
+								<div class="columns small-8 small-offset-1 space-50-left-right">
+									<div class="p space-150-top">To close your Gold loan and get your gold released, you need to bring the pledge receipt given to you by the gold loan provider.</div>
+									<div class="h5 medium space-50-top">Check your gold loan documents. Bring the Pledge Receipt.</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- CARD: 3 -->
+					<div class="card fill-light">
+						<div class="row">
+							<label class="card-head" tabindex="-1">
+								<div class="columns small-1"><span class="index h6 medium text-blue-2">3</span></div>
+								<div class="title h3 strong text-blue-2 columns small-10 space-50-left-right">Carry ID proof</div>
+								<div class="toggle columns small-1">
+									<input class="visuallyhidden" type="checkbox">
+									<div class="arrow"><span class="a1"></span><span class="a2"></span></div>
+								</div>
+							</label>
+						</div>
+						<div class="card-content">
+							<div class="row space-150-top">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-8 space-50-left-right"><div class="h5 medium">AADHAR Card</div></div>
+							</div>
+							<div class="row space-50-top-bottom">
+								<div class="inline-middle columns small-1 text-center"><div class="small medium text-uppercase">or</div></div>
+								<div class="inline-middle columns small-8 space-50-left space-100-right"><hr class="fill-light"></div>
+							</div>
+							<div class="row">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-8 space-50-left-right"><div class="h5 medium">Photo ID Proof</div></div>
+							</div>
+							<div class="row space-25-top">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-8 space-50-left-right"><div class="h5 medium">Address Proof</div></div>
+							</div>
+							<div class="row">
+								<div class="columns small-8 small-offset-1 space-50-left-right">
+									<div class="p space-150-top">Photo ID and Address proof is required.</div>
+									<div class="p space-50-top">Carry your Aadhar card for easy KYC check.</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="step-break columns small-12">
+						<div class="row text-neutral-3 space-75-left-right space-75-top">
+							<div class="label strong text-uppercase columns small-6">At our branch</div>
+							<div class="p strong columns small-6 text-right">&middot;&ensp;&middot;&ensp;&middot;</div>
+						</div>
+					</div>
+					<!-- CARD: 4 -->
+					<div class="card fill-light">
+						<div class="row">
+							<label class="card-head" tabindex="-1">
+								<div class="columns small-1"><span class="index h6 medium text-blue-2">4</span></div>
+								<div class="title h3 strong text-blue-2 columns small-10 space-50-left-right">KYC Verification</div>
+								<div class="toggle columns small-1">
+									<input class="visuallyhidden" type="checkbox">
+									<div class="arrow"><span class="a1"></span><span class="a2"></span></div>
+								</div>
+							</label>
+						</div>
+						<div class="card-content">
+							<div class="row space-150-top">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-8 space-50-left-right"><div class="h5 medium">Photo ID Proof</div></div>
+							</div>
+							<div class="row space-50-top">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-8 space-50-left-right">
+									<div class="h5 medium">Address Proof</div>
+									<div class="small space-25-top">We do a KYC check. Photo ID and Address Proof is a mandatory requirement. Your Aadhar Card has both.</div>
+								</div>
+							</div>
+							<div class="row space-50-top">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-8 space-50-left-right">
+									<div class="h5 medium">Verification Phone Call</div>
+									<div class="small space-25-top">As a fraud and safety feature, a verification phone call to your family is required.</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- CARD: 5 -->
+					<div class="card fill-yellow-2">
+						<div class="row">
+							<label class="card-head" tabindex="-1">
+								<div class="columns small-1"><span class="index h6 medium text-blue-2">5</span></div>
+								<div class="title h3 strong text-blue-2 columns small-10 space-50-left-right">Check Gold Rate</div>
+								<div class="toggle columns small-1">
+									<input class="visuallyhidden" type="checkbox">
+									<div class="arrow"><span class="a1"></span><span class="a2"></span></div>
+								</div>
+							</label>
+						</div>
+						<div class="card-content">
+							<div class="row space-150-top">
+								<div class="columns small-10 small-offset-1 space-50-left-right">
+									<div class="h5 medium space-150-bottom">Based on the gold purity check we will give you a quotation. Our rate is transparent. </div>
+									<a href class="button fill-dark">Check Live gold rate now</a>
+								</div>
+							</div>
+							<div class="row space-150-top">
+								<div class="columns small-12">
+									<a href="" class="watch-video block row fill-yellow-1" tabindex="-1">
+										<div class="columns small-6">
+											<div class="thumbnail" style="background-image: url(/* -- insert url here -- */);"></div>
+										</div>
+										<div class="columns small-6 space-50-left space-25-right">
+											<div class="title h6 medium space-25-top-bottom">Why is the gold rate lower than I expected?</div>
+											<div class="timestamp small">02:30</div>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="step-break columns small-12">
+						<div class="row text-neutral-3 space-75-left-right space-75-top">
+							<div class="label strong text-uppercase columns small-6">Visit Gold Loan Provider</div>
+							<div class="p strong columns small-6 text-right">&middot;&ensp;&middot;&ensp;&middot;</div>
+						</div>
+					</div>
+					<!-- CARD: 6 -->
+					<div class="card fill-light">
+						<div class="row">
+							<label class="card-head" tabindex="-1">
+								<div class="columns small-1"><span class="index h6 medium text-blue-2">6</span></div>
+								<div class="title h3 strong text-blue-2 columns small-10 space-50-left-right">Release your Gold</div>
+								<div class="toggle columns small-1">
+									<input class="visuallyhidden" type="checkbox">
+									<div class="arrow"><span class="a1"></span><span class="a2"></span></div>
+								</div>
+							</label>
+						</div>
+						<div class="card-content">
+							<div class="row space-150-top">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-10 space-50-left-right"><div class="h5 medium">Complete KYC</div></div>
+							</div>
+							<div class="row space-25-top">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-10 space-50-left-right"><div class="h5 medium">Lock-in Gold Rate</div></div>
+							</div>
+							<div class="row space-25-top space-150-bottom">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-10 space-50-left-right"><div class="h5 medium">Sign Term Sheet</div></div>
+							</div>
+							<div class="row space-150-bottom">
+								<div class="columns small-8 small-offset-1 space-50-left-right">
+									<div class="h5 medium">We will send our representative with you to close your Gold loan.</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="columns small-12">
+									<a href="" class="watch-video block row fill-neutral-1" tabindex="-1">
+										<div class="columns small-6">
+											<div class="thumbnail" style="background-image: url(/* -- insert url here -- */);"></div>
+										</div>
+										<div class="columns small-6 space-50-left space-25-right">
+											<div class="title h6 medium space-25-top-bottom">How will you get my gold released ?</div>
+											<div class="timestamp small">02:30</div>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="step-break columns small-12">
+						<div class="row text-neutral-3 space-75-left-right space-75-top">
+							<div class="label strong text-uppercase columns small-6">At our branch</div>
+							<div class="p strong columns small-6 text-right">&middot;&ensp;&middot;&ensp;&middot;</div>
+						</div>
+					</div>
+					<!-- CARD: 7 -->
+					<div class="card fill-light">
+						<div class="row">
+							<label class="card-head" tabindex="-1">
+								<div class="columns small-1"><span class="index h6 medium text-blue-2">7</span></div>
+								<div class="title h3 strong text-blue-2 columns small-10 space-50-left-right">Check Gold Purity</div>
+								<div class="toggle columns small-1">
+									<input class="visuallyhidden" type="checkbox">
+									<div class="arrow"><span class="a1"></span><span class="a2"></span></div>
+								</div>
+							</label>
+						</div>
+						<div class="card-content">
+							<div class="row space-150-top">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-10 space-50-left-right"><div class="h5 medium">Tamper Proof</div></div>
+							</div>
+							<div class="row space-25-top">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-10 space-50-left-right"><div class="h5 medium">German Machines</div></div>
+							</div>
+							<div class="row space-25-top space-150-bottom">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-10 space-50-left-right"><div class="h5 medium">Instant Valuation</div></div>
+							</div>
+							<div class="row">
+								<div class="columns small-12">
+									<a href="" class="watch-video block row fill-neutral-1" tabindex="-1">
+										<div class="columns small-6">
+											<div class="thumbnail" style="background-image: url(/* -- insert url here -- */);"></div>
+										</div>
+										<div class="columns small-6 space-50-left space-25-right">
+											<div class="title h6 medium space-25-top-bottom">How is gold purity checked?</div>
+											<div class="timestamp small">02:30</div>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- CARD: 8 -->
+					<div class="card fill-light">
+						<div class="row">
+							<label class="card-head" tabindex="-1">
+								<div class="columns small-1"><span class="index h6 medium text-blue-2">8</span></div>
+								<div class="title h3 strong text-blue-2 columns small-10 space-50-left-right">Instant Bank Transfer</div>
+								<div class="toggle columns small-1">
+									<input class="visuallyhidden" type="checkbox">
+									<div class="arrow"><span class="a1"></span><span class="a2"></span></div>
+								</div>
+							</label>
+						</div>
+						<div class="card-content">
+							<div class="row space-150-top">
+								<div class="columns small-9 small-offset-1 space-50-left-right">
+									<div class="p">For your security, we transfer the money to your bank account. You can confirm the money transfer before leaving our branch.</div>
+								</div>
+							</div>
+							<div class="row space-150-top">
+								<div class="columns small-1"><span class="checkmark h5 medium fill-blue-2 text-light">&check;</span></div>
+								<div class="columns small-10 space-50-left-right"><div class="h3 text-blue-2 medium">Your money is transferred instantly to your bank account.</div></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
