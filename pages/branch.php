a28cfeb5bd116439ccaa22fc8cfd47f4fa7ec283
MarioWindsor <mario@lazaro.in>
2021-06-24 19:14:31 +0530

    Branch Page: front-end build: p14

--- pages/branch.php
+++ pages/branch.php
@@ -23,11 +23,63 @@ require_once __ROOT__ . '/inc/header.php';
 	<div class="container">
 		<div class="row">
 			<div class="intro columns small-6 medium-4 large-3 space-100-bottom">
-				<div class="h2 strong text-blue-2">Find a WhiteGold</div>
-				<div class="h2 strong text-neutral-3">near you</div>
+				<div class="title h2 strong text-blue-2">Find a WhiteGold <br><span class="text-neutral-3">near you</span></div>
+				<div class="char"><img class="block" src="../media/character/char-7.png<?php echo $ver ?>"></div>
 			</div>
 			<div class="branch-listing columns small-12 medium-7 medium-offset-1 large-6 large-offset-3">
 				<div class="branch-grid">
+					<!-- Branch -->
+					<div class="branch fill-light">
+						<div class="thumbnail fill-neutral-1 radius-25" style="background-image: url(/*insert thumbnail url*/);"></div>
+						<div class="title h6 strong space-50-top-bottom">Kamanahalli</div>
+						<div class="distance h4 text-neutral-3">1.2km</div>
+						<div class="check-distance hidden small medium text-uppercase text-blue-1 space-25 fill-neutral-1">
+							<span class="material-icons inline-middle">my_location</span>
+							<span class="inline-middle">&nbsp;Check Distance</span>
+						</div>
+						<div class="timings p text-neutral-3 space-50-bottom">Open Mon to Fri</div>
+						<a class="gmaps-link button">Open in Maps <!-- google maps icon --></a>
+					</div>
+					<!-- END: Branch -->
+					<!-- Branch -->
+					<div class="branch fill-light">
+						<div class="thumbnail fill-neutral-1 radius-25" style="background-image: url(/*insert thumbnail url*/);"></div>
+						<div class="title h6 strong space-50-top-bottom">Kamanahalli</div>
+						<div class="distance h4 text-neutral-3">1.2km</div>
+						<div class="check-distance hidden small medium text-uppercase text-blue-1 space-25 fill-neutral-1">
+							<span class="material-icons inline-middle">my_location</span>
+							<span class="inline-middle">&nbsp;Check Distance</span>
+						</div>
+						<div class="timings p text-neutral-3 space-50-bottom">Open Mon to Fri</div>
+						<a class="gmaps-link button">Open in Maps <!-- google maps icon --></a>
+					</div>
+					<!-- END: Branch -->
+					<!-- Branch -->
+					<div class="branch fill-light">
+						<div class="thumbnail fill-neutral-1 radius-25" style="background-image: url(/*insert thumbnail url*/);"></div>
+						<div class="title h6 strong space-50-top-bottom">Kamanahalli</div>
+						<div class="distance h4 text-neutral-3">1.2km</div>
+						<div class="check-distance hidden small medium text-uppercase text-blue-1 space-25 fill-neutral-1">
+							<span class="material-icons inline-middle">my_location</span>
+							<span class="inline-middle">&nbsp;Check Distance</span>
+						</div>
+						<div class="timings p text-neutral-3 space-50-bottom">Open Mon to Fri</div>
+						<a class="gmaps-link button">Open in Maps <!-- google maps icon --></a>
+					</div>
+					<!-- END: Branch -->
+					<!-- Branch -->
+					<div class="branch fill-light">
+						<div class="thumbnail fill-neutral-1 radius-25" style="background-image: url(/*insert thumbnail url*/);"></div>
+						<div class="title h6 strong space-50-top-bottom">Kamanahalli</div>
+						<div class="distance h4 text-neutral-3">1.2km</div>
+						<div class="check-distance hidden small medium text-uppercase text-blue-1 space-25 fill-neutral-1">
+							<span class="material-icons inline-middle">my_location</span>
+							<span class="inline-middle">&nbsp;Check Distance</span>
+						</div>
+						<div class="timings p text-neutral-3 space-50-bottom">Open Mon to Fri</div>
+						<a class="gmaps-link button">Open in Maps <!-- google maps icon --></a>
+					</div>
+					<!-- END: Branch -->
 					<!-- Branch -->
 					<div class="branch fill-light">
 						<div class="thumbnail fill-neutral-1 radius-25" style="background-image: url(/*insert thumbnail url*/);"></div>
