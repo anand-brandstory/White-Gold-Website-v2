0e8caee8df95788c8911ab306c7c69b0544e671c
MarioWindsor <mario@lazaro.in>
2021-06-24 22:13:16 +0530

    Home Page: front-end: Bug: p16.1

--- pages/section/sell-gold.php
+++ pages/section/sell-gold.php
@@ -1,4 +1,4 @@
-<section class="sell-gold-section fill-blue-3 space-200-top">
+<section class="sell-gold-section fill-blue-3 space-200-top space-100-bottom">
 	<div class="container">
 		<div class="row">
 			<div class="intro columns small-12 large-4">
