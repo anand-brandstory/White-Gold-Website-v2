e3e5158f0c78251fe5cd3f71d6ed03c537f1eca1
adi <adi@lazaro.in>
2021-06-30 17:35:50 +0530

    Home page: Carousel: CMS-ify this

--- pages/section/landing-carousel.php
+++ pages/section/landing-carousel.php
@@ -1,36 +1,25 @@
+<?php
+
+use BFS\CMS;
+
+$allCarouselSlides = CMS::getPostsOf( 'card' );
+$carouselSlides = array_filter( $allCarouselSlides, function ( $slide ) {
+	return in_array( REGION, $slide->get( 'regions_applicable' ) );
+} );
+
+?>
 <section class="landing-carousel-section fill-blue-5" id="landing-carousel-section" data-section-title="Landing Carousel Section" data-section-slug="landing-carousel-section">
 	<div class="row carousel js_carousel_container" style="--fade-left: linear-gradient( to left, rgba(0, 50, 160, 0) 0%, rgba(0, 50, 160, 1) 50%); --fade-right: linear-gradient( to right, rgba(0, 50, 160, 0) 0%, rgba(0, 50, 160, 1) 50%);">
 		<div class="carousel-list js_carousel_content">
-			<!-- Card : 1 -->
-			<div class="row carousel-list-item js_carousel_item">
-				<div class="carousel-card container space-100-top-bottom" style="background-image: url('../media/cutout/char-4.png<?php echo $ver ?>'); background-position: bottom right;">
-					<div class="card-content content-block">
-						<h2><strong>India's largest</strong></h2>
-						<h4>gold buying company</h4>
+			<?php foreach ( $carouselSlides as $slide ) : ?>
+				<div class="row carousel-list-item js_carousel_item">
+					<div class="carousel-card container space-100-top-bottom" style="background-image: url( '<?= $slide->get( 'bg_image' ) . $ver ?>' ); background-position: <?= $slide->get( 'bg_image_anchor' ) ?>;">
+						<div class="card-content content-block">
+							<?= $slide->get( 'card_text' ) ?>
+						</div>
 					</div>
 				</div>
-			</div>
-			<!-- END: Card -->
-			<!-- Card : 2 -->
-			<div class="row carousel-list-item js_carousel_item">
-				<div class="carousel-card container space-100-top-bottom" style="background-image: url(/* -- insert url -- */); background-position: /* -- insert bg position -- */;">
-					<div class="card-content content-block">
-						<h2><strong>India's largest</strong></h2>
-						<h4>gold buying company</h4>
-					</div>
-				</div>
-			</div>
-			<!-- END: Card -->
-			<!-- Card : 3 -->
-			<div class="row carousel-list-item js_carousel_item">
-				<div class="carousel-card container space-100-top-bottom" style="background-image: url(/* -- insert url -- */); background-position: /* -- insert bg position -- */;">
-					<div class="card-content content-block">
-						<h2><strong>India's largest</strong></h2>
-						<h4>gold buying company</h4>
-					</div>
-				</div>
-			</div>
-			<!-- END: Card -->
+			<?php endforeach; ?>
 		</div>
 		<div class="carousel-controls clearfix">
 			<div class="prev float-left"><button class="carousel-button h3 js_pager" data-dir="left"><span class="country-code-divider material-icons" data-icon="arrow_back"></span></button></div>
