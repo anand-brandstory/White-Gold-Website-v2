da6bf48dca108d6f8c3b3cb0014a7d632b684cc2
adi <adi@lazaro.in>
2021-07-01 18:01:51 +0530

    Branch Finder page: Add "Check distance" and "Show Nearest Branch" features

--- js/pages/branch-finder.js
+++ js/pages/branch-finder.js
@@ -9,6 +9,16 @@ $( function () {
 if ( ! navigator.geolocation )
 	return;
 
+// For each branch in memory, store a reference to its corresponding DOM node
+var $branches = $( ".js_branch" );
+window.__BFS.data.branchesInRegion = window.__BFS.data.branchesInRegion.map( function ( branch, _index ) {
+	return Object.assign(
+		{ },
+		branch,
+		{ $node: $branches.eq( _index ) }
+	);
+} );
+
 // If user has not consented to sharing their geolocation, or has not been prompted, then reveal the "Check distance" buttons
 $( ".js_branch .js_check_distance" ).removeClass( "hidden" );
 
@@ -19,18 +29,93 @@ $( document ).on( "click", ".js_check_distance", function ( event ) {
 		var branches = window.__BFS.data.branchesInRegion;
 		// Calculate the distances between the user and the branches
 		branches = branches.map( function ( branch ) {
-			branch.distanceFromUser = calculateDistanceBetweenCoordinates( userGPSData, branch );
-			return branch;
+			return Object.assign(
+				{ },
+				branch,
+				{ distanceFromUser: calculateDistanceBetweenCoordinates( userGPSData, branch ) }
+			);
 		} );
 		// Plug in the calculated distances in the markup
-		$( ".js_branch" ).each( function ( _i, domEl ) {
-			var $branch = $( domEl );
-			var branch = branches[ _i ];
+		branches.forEach( function ( branch ) {
+			var $branch = $( branch.$node );
 			$branch.find( ".js_distance_from_user" ).text( branch.distanceFromUser ).removeClass( "hidden" );
 			$branch.find( ".js_check_distance" ).addClass( "hidden" );
 		} );
 	} );
 } );
+
+
+
+// If the user clicks on the "Show Nearest Branch" button, order the branches by distance
+$( document ).on( "click", ".js_order_branches", function ( event ) {
+
+	var $orderBranchesByNearestButton = $( event.target ).closest( ".js_order_branches" );
+	$orderBranchesByNearestButton.addClass( "loading" );
+
+	// 1. Get the user's geolocation
+	getCurrentUserGeoLocation().then( function ( userGPSData ) {
+
+		// 2. Calculate the distances between the user and the branches
+		var branches = window.__BFS.data.branchesInRegion;
+		branches = branches.map( function ( branch ) {
+			return Object.assign(
+				{ },
+				branch,
+				{ distanceFromUser: calculateDistanceBetweenCoordinates( userGPSData, branch ) }
+			);
+		} );
+
+		// 3. Hide the branches from the user
+		var $branchesContainer = $( ".js_branches_container" );
+		$branchesContainer.addClass( "fade-out" );
+			// Expand the list of branches and lock the height
+		$( ".js_more_branches" ).get( 0 ).checked = true;
+		$branchesContainer.css( { height: $branchesContainer.height() + "px" } );
+
+		setTimeout( function () {
+
+			// 4. Sort the branches
+			branches = branches.sort( function ( a, b ) {
+				return parseFloat( a.distanceFromUser ) - parseFloat( b.distanceFromUser );
+			} );
+
+			// 5. Plug in the calculated distances to the branches markup
+			branches.forEach( function ( branch ) {
+				var $branch = $( branch.$node );
+				$branch.find( ".js_distance_from_user" ).text( branch.distanceFromUser ).removeClass( "hidden" );
+				$branch.find( ".js_check_distance" ).addClass( "hidden" );
+			} );
+
+			// 6. Un-mount the branches from the DOM
+			branches.forEach( function ( branch ) {
+				branch.$node.detach();
+			} )
+
+			// 7. Re-mount the branches to the DOM
+			branches.forEach( function ( branch ) {
+				$branchesContainer.append( branch.$node );
+			} );
+
+			// 8. Hide the "Show Nearest Branch" button
+			$orderBranchesByNearestButton.addClass( "hidden" );
+
+			// 9. Reveal the branches to the user again
+			$branchesContainer.css( { height: "auto" } );
+			$branchesContainer.removeClass( "fade-out" );
+
+			// 10. Scroll to the top
+			window.scrollTo( {
+				top: $branchesContainer.offset().top,
+				left: 0,
+				behavior: "smooth"
+			} )
+
+		}, 100 );
+
+	} );
+
+} );
+
 
 
 
@@ -48,7 +133,8 @@ $( document ).on( "click", ".js_check_distance", function ( event ) {
  */
 function getCurrentUserGeoLocation () {
 	return getCurrentPosition()
-		.then( getLocationDetails );
+		.then( getLocationDetails )
+		.catch( handleErrorWhileAcquiringLocation )
 }
 
 	/*
@@ -87,9 +173,24 @@ function getCurrentUserGeoLocation () {
 	 * ----- Error handler: While attempting to acquire the user's location
 	 */
 	function handleErrorWhileAcquiringLocation ( e ) {
-		alert( "There was an issue in fetching your location. Please check your privacy settings for this website or try again after sometime." );
+		alert( "We were unable to fetch your location. Please refresh the page and try again, or check your privacy settings on your device for this website." );
 		console.log( e );
 	}
+
+
+/*
+ * ----- Given a reference points and a list of candidate points, this function returns the point that is the closest to the reference point
+ */
+function getNearest ( referenceCoordinates, candidateCoordinates ) {
+	return geolib.findNearest( referenceCoordinates, candidateCoordinates );
+}
+
+/*
+ * ----- Given a reference points and a list of candidate points, this function returns the list of candidate points ordered by distance.
+ */
+function orderByDistance ( referenceCoordinates, candidateCoordinates ) {
+	return geolib.orderByDistance( referenceCoordinates, candidateCoordinates );
+}
 
 /*
  * ----- Calculate the distance between two coordinates
@@ -100,4 +201,3 @@ function calculateDistanceBetweenCoordinates ( source, destination ) {
 	var distanceInWords = distanceInKm + "km";
 	return distanceInWords;
 }
-
