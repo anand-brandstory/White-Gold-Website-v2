b16478005fa0e902dba1fa2e163973376a1c97c1
adi <adi@lazaro.in>
2021-07-20 16:41:26 +0530

    Live Gold page: Set this up for the various regions

---
+++ pages/ka/live-gold.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ *
+ * This is the Karnataka-specific branch page.
+ *
+ */
+
+const REGION = 'ka';
+
+require_once __ROOT__ . '/pages/_live-gold.php';
---
+++ pages/kl/live-gold.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ *
+ * This is the Kerala-specific branch page.
+ *
+ */
+
+const REGION = 'kl';
+
+require_once __ROOT__ . '/pages/_live-gold.php';
