74412f424cbd1dbc579be2e3cad090086d9fed9c
MarioWindsor <mario@lazaro.in>
2021-07-22 16:29:32 +0530

    Front-end build: LiveGold Page: p32

--- pages/_live-gold.php
+++ pages/_live-gold.php
@@ -23,88 +23,15 @@ require_once __ROOT__ . '/inc/header.php';
 <!-- END: Header Section -->
 
 
-<!-- Live Gold Section -->
-<section class="live-gold-section fill-dark">
-	<div class="container space-200-top-bottom js_live_gold_form_section hidden">
-		<div class="row sell-gold-form">
-			<div class="columns small-9 medium-5 large-3">
-				<div class="form-card row fill-light">
-					<form class="form form-base js_live_gold_form" onsubmit="event.preventDefault()">
-						<div class="columns small-10 space-50-bottom">
-							<div class="h5 strong">Get a guaranteed ‘Gold Rate’ in any White Gold branch in your city</div>
-						</div>
-						<div class="columns small-12 space-50-top">
-							<label class="phone-verify form-label block">
-								<input type="text" class="form-input-field phone-number block" id="js_live_gold_form_input_phone">
-								<select class="form-input-field country-code js_phone_country_code">
-									<?php require __ROOT__ . '/inc/phone-country-codes.php' ?>
-								</select>
-								<input type="text" disabled="" class="form-input-field country-code-label js_phone_country_code_label" value="+91" id="js_live_gold_form_input_phone_country_code">
-								<span class="country-code-divider material-icons" data-icon="unfold_more"></span>
-								<span class="form-label-title medium fill-light cursor-pointer">Mobile Number</span>
-							</label>
-						</div>
-						<div class="row space-25-top space-50-left-right">
-							<div class="small text-neutral-3">I hereby authorise, WHITE GOLD to call me on this number.</div>
-						</div>
-						<div class="columns small-12 space-50-top">
-							<label class="form-label block">
-								<span class="form-label-title hidden medium fill-light cursor-pointer">Submit</span>
-								<button class="button fill-dark" type="submit">
-									<span class="button-label">Get OTP&ensp;</span>
-									<img class="button-icon tall" src="../media/icon/sms-tall-green.svg<?php echo $ver ?>">
-								</button>
-							</label>
-						</div>
-					</form>
-					<form class="form form-otp js_otp_form js_otp_form_live_gold" onsubmit="event.preventDefault()">
-						<div class="columns small-12">
-							<label class="form-label block">
-								<input type="text" placeholder="Enter OTP" class="form-input-field block" id="js_form_input_otp_live_gold">
-								<span class="form-label-title medium fill-light cursor-pointer">Enter OTP</span>
-							</label>
-						</div>
-						<div class="columns small-12 space-50-top">
-							<label class="form-label block">
-								<span class="form-label-title hidden medium fill-light cursor-pointer">Submit</span>
-								<button class="button fill-blue-1" type="submit">Submit OTP</button>
-							</label>
-						</div>
-					</form>
-					<div class="form form-thankyou">
-						<div class="columns small-12">
-							<div class="h4 strong space-50-bottom">Launching on <br>31st July, 2021.</div>
-							<div class="p">The Live Gold Rate Feature will ensure that you get the same gold rate across all our branches.</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="inline-modal">
-		<div class="container">
-			<div class="row">
-				<div class="otp-verify">
-					<!-- insert text -->
-				</div>
-				<div class="off-hrs">
-					<!-- insert text -->
-				</div>
-				<div class="holiday">
-					<!-- insert text -->
-				</div>
-				<div class="number-blocked">
-					<!-- insert text -->
-				</div>
-			</div>
-		</div>
-	</div>
+<!-- Live Gold Section :: Note : add a 'hide' class with corresponding message class like 'otp-verify-message' to display the appropriate message card -->
+<section class="live-gold-section hide otp-verify-message fill-dark">
+	<!-- Live Gold Content -->
 	<div class="live-gold space-200-top-bottom">
 		<div class="row">
 			<div class="container">
 				<div class="live-gold-data columns small-12 medium-5 large-4 space-200-bottom">
 					<div class="title h2 strong space-25-bottom">White Gold <br><span class="text-yellow-2">Live Gold Rate</span></div>
-					<div class="date label space-150-bottom">12 June 2021&ensp;&bull;&ensp;9:45:00 AM IST</div>
+					<div class="timestamp label space-150-bottom"><span class="date inline">12 June 2021</span>&ensp;&bull;&ensp;<span class="time inline">9:45:00 AM</span> IST</div>
 					<div class="data inline">
 						<div class="24k space-50-bottom">
 							<div class="h6 text-yellow-2">24 Karat Gold</div>
@@ -150,7 +77,7 @@ require_once __ROOT__ . '/inc/header.php';
 							</div>
 							<div class="columns small-12 space-50-top space-100-bottom">
 								<label class="form-label block">
-									<select class="block" id="purity">
+									<select class="form-input-field block" id="purity">
 										<option value="24">24 Karat</option>
 										<option value="22">22 Karat</option>
 									</select>
@@ -191,6 +118,131 @@ require_once __ROOT__ . '/inc/header.php';
 		</div>
 		<div class="background row fill-yellow-2" style="background-image: linear-gradient(0deg, rgba(255,201,128,0.4) 40%, rgba(33,35,34,1) 98%), url('/media/background/sell-gold.png<?= $ver ?>');"></div>
 	</div>
+	<!-- End: Live Gold Content -->
+	<!-- Hide Live Gold Messages -->
+	<div class="hide-live-gold space-200-top-bottom js_live_gold_form_section">
+		<div class="row">
+			<div class="container">
+				<div class="otp-verify message columns small-9 medium-5 large-3">
+					<div class="form-card row fill-light">
+						<form class="form form-base js_live_gold_form" onsubmit="event.preventDefault()">
+							<div class="columns small-10 space-50-bottom">
+								<div class="h5 strong">Get a guaranteed ‘Gold Rate’ in any White Gold branch in your city</div>
+							</div>
+							<div class="columns small-12 space-50-top">
+								<label class="phone-verify form-label block">
+									<input type="text" class="form-input-field phone-number block" id="js_live_gold_form_input_phone">
+									<select class="form-input-field country-code js_phone_country_code">
+										<?php require __ROOT__ . '/inc/phone-country-codes.php' ?>
+									</select>
+									<input type="text" disabled="" class="form-input-field country-code-label js_phone_country_code_label" value="+91" id="js_live_gold_form_input_phone_country_code">
+									<span class="country-code-divider material-icons" data-icon="unfold_more"></span>
+									<span class="form-label-title medium fill-light cursor-pointer">Mobile Number</span>
+								</label>
+							</div>
+							<div class="row space-25-top space-50-left-right">
+								<div class="small text-neutral-3">I hereby authorise, WHITE GOLD to call me on this number.</div>
+							</div>
+							<div class="columns small-12 space-50-top">
+								<label class="form-label block">
+									<span class="form-label-title hidden medium fill-light cursor-pointer">Submit</span>
+									<button class="button fill-dark" type="submit">
+										<span class="button-label">Get OTP&ensp;</span>
+										<img class="button-icon tall" src="../media/icon/sms-tall-green.svg<?php echo $ver ?>">
+									</button>
+								</label>
+							</div>
+						</form>
+						<form class="form form-otp js_otp_form js_otp_form_live_gold" onsubmit="event.preventDefault()">
+							<div class="columns small-12">
+								<label class="form-label block">
+									<input type="text" placeholder="Enter OTP" class="form-input-field block" id="js_form_input_otp_live_gold">
+									<span class="form-label-title medium fill-light cursor-pointer">Enter OTP</span>
+								</label>
+							</div>
+							<div class="columns small-12 space-50-top">
+								<label class="form-label block">
+									<span class="form-label-title hidden medium fill-light cursor-pointer">Submit</span>
+									<button class="button fill-blue-1" type="submit">Submit OTP</button>
+								</label>
+							</div>
+						</form>
+						<div class="form form-thankyou">
+							<div class="columns small-12">
+								<div class="h4 strong space-50-bottom">Launching on <br>31st July, 2021.</div>
+								<div class="p">The Live Gold Rate Feature will ensure that you get the same gold rate across all our branches.</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="off-hrs message columns small-9 medium-5 large-3">
+					<div class="form-card row fill-light">
+						<form class="form form-base" onsubmit="event.preventDefault()">
+							<div class="columns small-10 space-50-bottom">
+								<div class="h5 strong space-25-bottom">We are open between Monday to Saturday from 9:30 am to 6:30 pm.</div>
+								<div class="p">Check back at the appropriate time. Sorry for the inconvenience.</div>
+							</div>
+							<div class="columns small-12 space-75-top">
+								<a href="/" class="button fill-blue-1">
+									<span class="button-label">Back to Homepage&ensp;</span>
+									<img class="button-icon tall" src="../media/icon/back-tall-blue.svg<?php echo $ver ?>">
+								</a>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div class="holiday message columns small-9 medium-5 large-3">
+					<div class="form-card row fill-light">
+						<form class="form form-base" onsubmit="event.preventDefault()">
+							<div class="columns small-10 space-50-bottom">
+								<div class="h5 strong space-25-bottom">We are closed at this time due to "Insert Reason eg:Public Holiday Name".</div>
+								<div class="p">Check back again later. Sorry for the inconvenience.</div>
+							</div>
+							<div class="columns small-12 space-75-top">
+								<a href="/" class="button fill-blue-1">
+									<span class="button-label">Back to Homepage&ensp;</span>
+									<img class="button-icon tall" src="../media/icon/back-tall-blue.svg<?php echo $ver ?>">
+								</a>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div class="end-session message columns small-9 medium-5 large-3">
+					<div class="form-card row fill-light">
+						<form class="form form-base" onsubmit="event.preventDefault()">
+							<div class="columns small-10 space-50-bottom">
+								<div class="h4 strong space-25-bottom">Your session has expired.</div>
+								<div class="p">Check back again later. Thank You.</div>
+							</div>
+							<div class="columns small-12 space-75-top">
+								<a href="/" class="button fill-blue-1">
+									<span class="button-label">Back to Homepage&ensp;</span>
+									<img class="button-icon tall" src="../media/icon/back-tall-blue.svg<?php echo $ver ?>">
+								</a>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div class="number-blocked message columns small-9 medium-5 large-3">
+					<div class="form-card row fill-light">
+						<form class="form form-base" onsubmit="event.preventDefault()">
+							<div class="columns small-10 space-50-bottom">
+								<div class="h5 strong space-25-bottom">Your phone number has been flagged for overuse.</div>
+								<div class="p">Get in touch with us directly to unblock your phone number. Sorry for the inconvenience.</div>
+							</div>
+							<div class="columns small-12 space-75-top">
+								<a href="/" class="button fill-blue-1">
+									<span class="button-label">Back to Homepage&ensp;</span>
+									<img class="button-icon tall" src="../media/icon/back-tall-blue.svg<?php echo $ver ?>">
+								</a>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End: Hide Live Gold Messages -->
 </section>
 <!-- END: Live Gold Section -->
 
