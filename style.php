b2bf9f0c2bf49facb032c56c7428113ab2a06e89
MarioWindsor <mario@lazaro.in>
2021-08-27 15:05:59 +0530

    Front-end: FAQs Page: Initialise Styles: P1

--- style.php
+++ style.php
@@ -16,6 +16,7 @@
 <link rel="stylesheet" type="text/css" href="/css/pages/page.css<?php echo $ver ?>">
 <link rel="stylesheet" type="text/css" href="/css/pages/home-page.css<?php echo $ver ?>">
 <link rel="stylesheet" type="text/css" href="/css/pages/post-page.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/faqs-page.css<?php echo $ver ?>">
 <link rel="stylesheet" type="text/css" href="/css/pages/modal/modal-menu.css<?php echo $ver ?>">
 <link rel="stylesheet" type="text/css" href="/css/pages/section/sample-section.css<?php echo $ver ?>">
 <link rel="stylesheet" type="text/css" href="/css/pages/section/sell-gold-section.css<?php echo $ver ?>">
