a1fe957c39cc8070573954291cf41a5666654cca
adi <52442230+adi-lazaro@users.noreply.github.com>
2023-04-15 15:05:01 +0530

    Cleanup: Modals: Remove unused ones

--- pages/snippet/modals.php
+++ pages/snippet/modals.php
@@ -17,33 +17,6 @@
 			</div>
 		</div><!-- END : YouTube Video -->
 
-		<!-- Modal Content : Sample Form -->
-		<?php /*
-		<div class="modal-box-content js_modal_box_content" data-mod-id="sample-form">
-			<div class="container">
-				<div class="row">
-					<div class="columns small-12">
-						<h2>Form Title Goes Here</h2>
-					</div>
-				</div>
-				<div class="row">
-					<!-- video embed -->
-					<div class="columns small-12">
-						<div class="video-embed js_video_embed" data-src="lncVHzsc_QA">
-							<div class="video-loading-indicator"></div>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="columns small-12">
-						<!-- <p>Sample text</p> -->
-					</div>
-				</div>
-			</div>
-		</div><!-- END : Sample Form -->
-		*/ ?>
-
-
 		<!-- Modal Close Button -->
 		<div class="modal-close js_modal_close">&times;</div>
 	</div>
